6e1bd3c18c219987c05835f73598fa753ca0be1a
sakshisingh57 <sakshisingh57@deloitte.com>
2023-09-25 13:07:17 +0530

    first commit

---
+++ sampleSpringBoot/src/test/java/com/example/sampleSpringBoot/SampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
